6f7b25cd592d53205d98e0a70da71455271716ca
Jakub <jklimasz@gmail.com>
2019-07-30 20:18:58 +0200

    Added file info stucture

---
+++ update.php
@@ -0,0 +1,26 @@
+{
+  "status": 200,
+  "message": "Thanks for supporting UFPV",
+  "files": [
+    {
+      "type": "firmware",
+      "name": "[NV14-2.1.5] Nighty 2019.07.29.bin",
+      "fileName": "[NV14-2.1.5] Nighty 2019.07.29.bin",
+      "version": "",
+      "md5Sum": "ce1bd4a5a7cdbfeaae9c5062fd8cc056",
+      "size": 1789416,
+      "startAddress": 134217728,
+      "url": "https://open-flysky.github.io/Nighty_2019_07_29.bin"
+    },
+    
+    {
+      "type": "file",
+      "name": "[NV14-2.1.5] SD Card image",
+      "fileName": "[NV14-2.1.5] SD Card image.zip",
+      "version": "",
+      "md5Sum": "358ca1a636dd9c2ab2ea17b4cfe0cb12",
+      "size":  2192094,
+      "url": "https://open-flysky.github.io/SD_card_2.1.15_218.zip"
+    },
+  ]
+}
