7cbcf9ef85faecbea1042cb27bc15bd4913ecef4
Jakub <jklimasz@gmail.com>
2019-07-30 20:21:59 +0200

    Fixed json syntax

--- update.php
+++ update.php
@@ -12,7 +12,6 @@
       "startAddress": 134217728,
       "url": "https://open-flysky.github.io/Nighty_2019_07_29.bin"
     },
-    
     {
       "type": "file",
       "name": "[NV14-2.1.5] SD Card image",
@@ -21,6 +20,6 @@
       "md5Sum": "358ca1a636dd9c2ab2ea17b4cfe0cb12",
       "size":  2192094,
       "url": "https://open-flysky.github.io/SD_card_2.1.15_218.zip"
-    },
+    }
   ]
 }
